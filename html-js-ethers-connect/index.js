069270c51ea6c31641631493562ce3edc3f70e12
crazyAT8 <88wwatson@gmail.com>
2024-09-02 09:56:55 -0500

    init 9/2/24

---
+++ html-js-ethers-connect/index.js
@@ -0,0 +1,15 @@
+const { ethers } = reqire("ethers");
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+    // console.log("We see metamask!");
+        await ethereum.request({ method: "eth_requestAccounts" });
+    }
+}
+
+async function execute() {}
+
+module.exports = {
+    connect,
+    execute
+}
